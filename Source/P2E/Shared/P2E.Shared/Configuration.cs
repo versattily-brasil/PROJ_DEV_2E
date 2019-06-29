a7d70a4d07d1dc56f728891401bba20aa14296aa
Júlio <julio.cesar@versattily.com.br>
2019-06-28 22:21:26 -0300

    Atualizando para .Net Core 2.2

--- Source/P2E/Shared/P2E.Shared/Configuration.cs
+++ Source/P2E/Shared/P2E.Shared/Configuration.cs
@@ -6,6 +6,6 @@ namespace P2E.Shared
 {
     public static class Configuration
     {
-        public static string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Users\julio.bicharra\Downloads\P2E\P2E\Shared\P2E.Shared\DBTemp\DBTemp.mdf;Integrated Security=True;Connect Timeout=30";
+        public static string ConnectionString = @"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C:\Projetos\Versattily\Git\P2E\Source\P2E\Shared\P2E.Shared\DBLocal\DbLocal.mdf;Integrated Security=True;Connect Timeout=30";
     }
 }
