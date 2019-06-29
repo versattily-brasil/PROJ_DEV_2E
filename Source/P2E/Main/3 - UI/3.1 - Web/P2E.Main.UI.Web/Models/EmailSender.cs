c21fe45e923f3f256c36aad834c0079289a396ee
Rafael Martins <rafcmmartins@gmail.com>
2019-06-28 22:46:36 -0400

    Adição inicial template SmartAdmin

---
+++ Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/Models/EmailSender.cs
@@ -0,0 +1,10 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity.UI.Services;
+
+namespace P2E.Main.UI.Web.Models
+{
+    public class EmailSender : IEmailSender
+    {
+        public Task SendEmailAsync(string email, string subject, string message) => Task.CompletedTask;
+    }
+}
--- Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/Models/ErrorViewModel.cs
+++ Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/Models/ErrorViewModel.cs
@@ -1,11 +1,8 @@
-using System;
-
-namespace P2E.Main.UI.Web.Models
+﻿namespace P2E.Main.UI.Web.Models
 {
     public class ErrorViewModel
     {
         public string RequestId { get; set; }
-
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
     }
 }
