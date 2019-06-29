ef15edddff53440968124ecc9ae4b5d546de03ac
Rafael Martins <rafcmmartins@gmail.com>
2019-06-29 17:01:13 -0400

    Adicionando lista/novo Exemplo

---
+++ Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/Controllers/ExemploController.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using P2E.Main.API.ViewModel;
+
+namespace P2E.Main.UI.Web.Controllers
+{
+    public class ExemploController : Controller
+    {
+        [HttpGet]
+        public IActionResult Lista()
+        {
+            HttpClient client = new HttpClient();
+            var result = client.GetAsync("http://localhost:50761/api/v1/exemplo").Result;
+
+            List<ExemploVM> list = new List<ExemploVM>();
+
+            if (result.IsSuccessStatusCode)
+            {
+                list = result.Content.ReadAsAsync<List<ExemploVM>>().Result;
+            }
+
+            return View(list);
+        }
+
+        [HttpGet]
+        public IActionResult Novo()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult Novo(ExemploVM exemplo)
+        {
+            HttpClient client = new HttpClient();
+            client.PostAsJsonAsync<ExemploVM>("http://localhost:50761/api/v1/exemplo", exemplo);
+
+            return RedirectToAction("Lista");
+        }
+    }
+}
