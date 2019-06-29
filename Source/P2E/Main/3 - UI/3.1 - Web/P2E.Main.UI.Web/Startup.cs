a31d7a66f97e6f0c5fc5364e989cefb539513eee
Júlio <julio.cesar@versattily.com.br>
2019-06-28 23:10:05 -0300

    Adicionando projeto main.web

--- Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/Startup.cs
+++ Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/Startup.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
@@ -32,7 +32,7 @@ namespace P2E.Main.UI.Web
             });
 
 
-            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_1);
+            services.AddMvc().SetCompatibilityVersion(CompatibilityVersion.Version_2_2);
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -44,7 +44,8 @@ namespace P2E.Main.UI.Web
             }
             else
             {
-                app.UseExceptionHandler("/Error");
+                app.UseExceptionHandler("/Home/Error");
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
 
@@ -52,7 +53,12 @@ namespace P2E.Main.UI.Web
             app.UseStaticFiles();
             app.UseCookiePolicy();
 
-            app.UseMvc();
+            app.UseMvc(routes =>
+            {
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
         }
     }
 }
