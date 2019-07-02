ab2c3a0953153a6cc10f97f34decf15f9f9a1ca5
Júlio Cesar <julio.cesar@versattily.com.br>
2019-07-02 16:17:19 -0300

    desenvolvimento do gateway

---
+++ Source/P2E/Shared/P2E.Shared/ValuesObject/Name.cs
@@ -0,0 +1,30 @@
+﻿using FluentValidator;
+using FluentValidator.Validation;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2E.Shared.ValuesObject
+{
+    public class Name : Notifiable
+    {
+        public Name(string firstName, string lastName)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+
+            AddNotifications(
+                new ValidationContract()
+                .Requires()
+                .HasMinLen(FirstName, 3, "FirstName", "O nome deve conter no mínimo 3 caracteres")
+                .HasMaxLen(FirstName, 30, "FirstName", "O nome deve conter no mácimo 30 caracteres")
+                .HasMinLen(LastName, 3, "LastName", "O sobrenome deve conter no mínimo 3 caracteres")
+                .HasMaxLen(LastName, 30, "LastName", "O sobrenome deve conter no mácimo 30 caracteres")
+            );
+        }
+        public string FirstName { get; private set; }
+        public string LastName { get; private set; }
+
+        public override string ToString() => $"{FirstName}{LastName}";
+    }
+}
