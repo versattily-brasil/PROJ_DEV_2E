a6e7b9f0ce7a15bdae427f113b1ee6c54b0f0ad8
Júlio Cesar <julio.cesar@versattily.com.br>
2019-07-24 13:17:15 -0300

    Adicionando PageControl.cs

---
+++ Source/P2E/Shared/P2E.Shared/Model/PageControl.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2E.Shared.Model
+{
+    public interface IPageControl
+    {
+       int PageSize { get; set; }
+       int CurrentPage { get; set; }
+       int TotalRows { get; set; }
+    }
+}
