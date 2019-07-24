35ca4c1891882e8cea56c2e90da51dfc9ee91cea
Júlio <julio.cesar@versattily.com.br>
2019-07-23 23:34:30 -0300

    Inclusão da entidade e interface de repositorio PAR_NEGO

---
+++ Source/P2E/SSO/0 - Domain/P2E.SSO.Domain/Repositories/IPAR_NEGORepository.cs
@@ -0,0 +1,13 @@
+﻿using MicroOrm.Dapper.Repositories;
+using P2E.SSO.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2E.SSO.Domain.Repositories
+{
+    public interface IPAR_NEGORepository : IDapperRepository<PAR_NEGO>
+    {
+        IEnumerable<PAR_NEGO> GetByPage(int page = 1, int pageSize = 10);
+    }
+}
