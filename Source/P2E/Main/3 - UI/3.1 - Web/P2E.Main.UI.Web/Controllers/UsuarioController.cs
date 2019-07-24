f610f95305596c52dd9ac3d3656ed557bd689f14
Rafael Martins <rafcmmartins@gmail.com>
2019-07-23 22:18:29 -0400

    Adição front-end Usuários

---
+++ Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/Controllers/UsuarioController.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using P2E.Main.UI.Web.Extensions.Alerts;
+using P2E.Main.UI.Web.Models;
+using P2E.SSO.API.ViewModel;
+
+namespace P2E.Main.UI.Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly AppSettings appSettings;
+
+        public UsuarioController(AppSettings appSettings)
+        {
+            this.appSettings = appSettings;
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Lista()
+        {
+            HttpClient client = new HttpClient();
+            var result = await client.GetAsync(this.appSettings.ApiUsuarioBaseURL + "/usuario");
+            result.EnsureSuccessStatusCode();
+            List<UsuarioVM> list = await result.Content.ReadAsAsync<List<UsuarioVM>>();
+
+            return View(list);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Cadastro(int id)
+        {
+            if (id != 0)
+            {
+                HttpClient client = new HttpClient();
+                var result = await client.GetAsync(this.appSettings.ApiUsuarioBaseURL + "/usuario/" + id);
+                result.EnsureSuccessStatusCode();
+
+                UsuarioVM usuario = await result.Content.ReadAsAsync<UsuarioVM>();
+
+                return View(usuario);
+            }
+
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Cadastro(UsuarioVM usuario)
+        {
+            //if (exemplo.Descricao == String.Empty || exemplo.Valor <= 0)
+            //{
+            //    return View(exemplo).WithDanger("Erro.", "Preencha todos os campos.");
+            //}
+
+            HttpClient client = new HttpClient();
+            await client.PutAsJsonAsync<UsuarioVM>(this.appSettings.ApiUsuarioBaseURL + "/usuario/" + usuario.CD_USR, usuario);
+            return RedirectToAction("Lista").WithSuccess("Sucesso.", "O Usuário foi salvo corretamente.");
+        }
+
+        public async Task<IActionResult> Excluir(int Id)
+        {
+            HttpClient client = new HttpClient();
+            await client.DeleteAsync(this.appSettings.ApiUsuarioBaseURL + "/usuario/" + Id);
+            return RedirectToAction("Lista").WithSuccess("Sucesso.", "O Usuário foi excluído corretamente.");
+
+        }
+    }
+}
--- Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/Models/AppSettings.cs
+++ Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/Models/AppSettings.cs
@@ -8,5 +8,6 @@ namespace P2E.Main.UI.Web.Models
     public class AppSettings
     {
         public string ApiBaseURL { get; set; }
+        public string ApiUsuarioBaseURL { get; set; }
     }
 }
