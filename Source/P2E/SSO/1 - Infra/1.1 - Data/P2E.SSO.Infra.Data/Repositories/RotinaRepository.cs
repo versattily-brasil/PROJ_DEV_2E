ca31fbef8d9d0e0d82946ccc766cf0dca60835c0
Leonardo Ayres <leo.ayres@versattily.com.br>
2019-07-25 08:19:42 +0100

    First commit.

---
+++ Source/P2E/SSO/1 - Infra/1.1 - Data/P2E.SSO.Infra.Data/Repositories/RotinaRepository.cs
@@ -0,0 +1,17 @@
+﻿using MicroOrm.Dapper.Repositories;
+using P2E.SSO.Domain.Entities;
+using P2E.SSO.Domain.Repositories;
+using P2E.SSO.Infra.Data.DataContext;
+
+namespace P2E.SSO.Infra.Data.Repositories
+{
+    public class RotinaRepository : DapperRepository<Rotina>, IRotinaRepository
+    {
+        private readonly SSOContext _ssoContext;
+
+        public RotinaRepository(SSOContext ssoContext) : base(ssoContext.Connection)
+        {
+            _ssoContext = ssoContext;
+        }
+    }
+}
---
+++ Source/P2E/SSO/2 - API/P2E.SSO.API/ViewModel/RotinaVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace P2E.SSO.API.ViewModel
+{
+    /// <summary>
+    /// Classe de apresentação de Rotina na View
+    /// </summary>
+    public class RotinaVM
+    {
+        public int CD_ROT { get; set; }
+        public string TX_NOME { get; set; }
+        public string TX_DSC { get; set; }        
+        public int OP_TIPO { get; set; }
+    }
+}
