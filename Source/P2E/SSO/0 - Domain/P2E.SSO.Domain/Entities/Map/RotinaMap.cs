1614b7aa47f4d0e6c9ffa019bec3484bc6d359f1
Leonardo Ayres <leonardo.ayres@gmail.com>
2019-07-25 23:14:54 +0100

    First commit.

---
+++ Source/P2E/SSO/0 - Domain/P2E.SSO.Domain/Entities/Map/RotinaMap.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using DapperExtensions.Mapper;
+
+namespace P2E.SSO.Domain.Entities.Map
+{
+    public class RotinaMap : ClassMapper<Rotina>
+    {
+        public RotinaMap()
+        {
+            Table("TB_ROT");
+
+            Map(p => p.CD_ROT).Column("CD_ROT").Key(KeyType.Identity);
+            Map(p => p.TX_NOME).Column("TX_NOME");
+            Map(p => p.TX_DSC).Column("TX_DSC");
+            Map(p => p.OP_TIPO).Column("OP_TIPO");
+        }
+    }
+}
