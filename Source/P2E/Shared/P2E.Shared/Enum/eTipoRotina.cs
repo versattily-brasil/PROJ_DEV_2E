a9593e035a8d617530fff04fb8c679411d665e99
Leonardo Ayres <leonardo.ayres@gmail.com>
2019-07-28 15:53:59 +0100

    Primeiro commit. Enumeração para Tipos de Rotina.

---
+++ Source/P2E/Shared/P2E.Shared/Enum/eTipoRotina.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2E.Shared.Enum
+{
+    public enum eTipoRotina
+    {
+        Cadastrar = 1,
+        Alterar = 2,
+        Listar = 3,
+        Remover = 4
+    }
+}
