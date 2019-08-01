788b8b3547e688ba0ef47b9003c0435187eaa972
Leonardo Ayres <leonardo.ayres@gmail.com>
2019-08-02 00:46:51 +0100

    Novas classes para o Refactory do novo padrão.

---
+++ Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/Models/SSO/Rotina/RotinaListViewModel.cs
@@ -0,0 +1,15 @@
+﻿using P2E.Shared.Model;
+
+namespace P2E.Main.UI.Web.Models.SSO.Rotina
+{
+    public class RotinaListViewModel
+    {
+        public RotinaListViewModel()
+        {
+            DataPage = new DataPage<P2E.SSO.Domain.Entities.Rotina>();
+        }
+        public string nome { get; set; }
+        public string descricao { get; set; }
+        public DataPage<P2E.SSO.Domain.Entities.Rotina> DataPage { get; set; }
+    }
+}
---
+++ Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/Models/SSO/Rotina/RotinaViewModel.cs
@@ -0,0 +1,15 @@
+﻿using P2E.Shared.Enum;
+
+namespace P2E.Main.UI.Web.Models.SSO.Rotina
+{
+    /// <summary>
+    /// Classe de apresentação de Rotina na View
+    /// </summary>
+    public class RotinaViewModel
+    {
+        public int CD_ROT { get; set; }
+        public string TX_NOME { get; set; }
+        public string TX_DSC { get; set; }        
+        public eTipoRotina OP_TIPO { get; set; }
+    }
+}
