ce6904302cddcd0bedc6c5d68bb68bcbdd6b4f04
Leonardo Ayres <leonardo.ayres@gmail.com>
2019-08-02 00:32:45 +0100

    Refactory.

--- Source/P2E/SSO/0 - Domain/P2E.SSO.Domain/Repositories/IRotinaRepository.cs
+++ Source/P2E/SSO/0 - Domain/P2E.SSO.Domain/Repositories/IRotinaRepository.cs
@@ -1,10 +1,15 @@
 ﻿using System.Collections.Generic;
+using DapperExtensions;
 using MicroOrm.Dapper.Repositories;
+using P2E.Shared.Model;
 using P2E.SSO.Domain.Entities;
 
 namespace P2E.SSO.Domain.Repositories
 {
     public interface IRotinaRepository : IDapperRepository<Rotina>
     {
+        DataPage<Rotina> GetByPage(DataPage<Rotina> page, string descricao, string nome);
+
+        int GetTotalRows(PredicateGroup predicateGroup);
     }
 }
