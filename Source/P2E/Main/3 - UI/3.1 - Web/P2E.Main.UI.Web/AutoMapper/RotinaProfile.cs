5b6f197ed3bfd42b083354af185360c503a96321
Leonardo Ayres <leonardo.ayres@gmail.com>
2019-08-02 00:59:22 +0100

    Adicionado - Refactory

---
+++ Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/AutoMapper/RotinaProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using P2E.Main.UI.Web.Models.SSO.Rotina;
+using P2E.SSO.Domain.Entities;
+
+namespace P2E.Main.UI.Web.AutoMapper
+{
+    public class RotinaProfile : Profile
+    {
+        public RotinaProfile()
+        {
+            CreateMap<Rotina, RotinaViewModel>(MemberList.None);
+            CreateMap<RotinaViewModel, Rotina>(MemberList.Source);
+            AllowNullDestinationValues = true;
+        }
+    }
+}
