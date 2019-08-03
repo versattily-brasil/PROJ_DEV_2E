de780f24dcf81e21986c12e0b020fcd14cee83a3
Leonardo Ayres <leonardo.ayres@gmail.com>
2019-08-03 01:10:04 +0100

    Adicionado Profile para as classes de Operação.

---
+++ Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/AutoMapper/OperacaoProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using P2E.Main.UI.Web.Models.SSO.Operacao;
+using P2E.SSO.Domain.Entities;
+
+namespace P2E.Main.UI.Web.AutoMapper
+{
+    public class OperacaoProfile : Profile
+    {
+        public OperacaoProfile()
+        {
+            CreateMap<Operacao, OperacaoViewModel>(MemberList.None);
+            CreateMap<OperacaoViewModel, Operacao>(MemberList.Source);
+            AllowNullDestinationValues = true;
+        }
+    }
+}
