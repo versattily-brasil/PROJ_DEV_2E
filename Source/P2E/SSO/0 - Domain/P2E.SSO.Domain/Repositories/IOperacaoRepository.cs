31961fc7ed03b8ccf4050c18ba3c4a55d81f75ba
Leonardo Ayres <leonardo.ayres@gmail.com>
2019-08-03 01:26:51 +0100

    Adicionada interface para o repositório de Operação.

---
+++ Source/P2E/SSO/0 - Domain/P2E.SSO.Domain/Repositories/IOperacaoRepository.cs
@@ -0,0 +1,14 @@
+﻿using DapperExtensions;
+using MicroOrm.Dapper.Repositories;
+using P2E.Shared.Model;
+using P2E.SSO.Domain.Entities;
+
+namespace P2E.SSO.Domain.Repositories
+{
+    public interface IOperacaoRepository : IDapperRepository<Operacao>
+    {
+        DataPage<Operacao> GetByPage(DataPage<Operacao> page, string descricao);
+
+        int GetTotalRows(PredicateGroup predicateGroup);
+    }
+}
