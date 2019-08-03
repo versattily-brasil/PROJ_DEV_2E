d5aab298b8990e62fbb4a5c5aebfba5c53c15d7e
Leonardo Ayres <leonardo.ayres@gmail.com>
2019-08-03 01:14:34 +0100

    Adicionadas classes de ViewModel para Operação.

---
+++ Source/P2E/Main/3 - UI/3.1 - Web/P2E.Main.UI.Web/Models/SSO/Operacao/OperacaoViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace P2E.Main.UI.Web.Models.SSO.Operacao
+{
+    /// <summary>
+    /// Classe de apresentação de Operação na View
+    /// </summary>
+    public class OperacaoViewModel
+    {
+        public int CD_OPR { get; set; }        
+        public string TX_DSC { get; set; }        
+    }
+}
