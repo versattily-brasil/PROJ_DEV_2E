f28d3730cb5ba7f182b6ad3b1f584117f40c3969
JorgeKleber007 <jorge.rabelo@versattily.com.br>
2019-08-04 15:48:42 -0400

    Implementações das rotinas de listagem de servico e modulo por parceiro

---
+++ Source/P2E/SSO/0 - Domain/P2E.SSO.Domain/Entities/Map/ParceiroNegocioModuloMap.cs
@@ -0,0 +1,20 @@
+﻿using DapperExtensions.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2E.SSO.Domain.Entities.Map
+{
+    public class ParceiroNegocioModuloMap : ClassMapper<ParceiroNegocioModulo>
+    {
+        public ParceiroNegocioModuloMap()
+        {
+            Table("TB_PAR_SRV_MOD");
+
+            Map(p => p.CD_PAR_SRV_MOD).Column("CD_PAR_SRV_MOD").Key(KeyType.Identity);
+            Map(p => p.CD_PAR).Column("CD_PAR");
+            Map(p => p.CD_SRV).Column("CD_SRV");
+            Map(p => p.CD_MOD).Column("CD_MOD");
+        }
+    }
+}
---
+++ Source/P2E/SSO/1 - Infra/1.1 - Data/P2E.SSO.Infra.Data/Repositories/ParceiroNegocioModuloRepository.cs
@@ -0,0 +1,39 @@
+﻿using Dapper;
+using MicroOrm.Dapper.Repositories;
+using P2E.SSO.Domain.Entities;
+using P2E.SSO.Domain.Repositories;
+using P2E.SSO.Infra.Data.DataContext;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Text;
+
+namespace P2E.SSO.Infra.Data.Repositories
+{
+    public class ParceiroNegocioModuloRepository : DapperRepository<ParceiroNegocioModulo>, IParceiroNegocioModuloRepository
+    {
+        private readonly SSOContext _context;
+
+        public ParceiroNegocioModuloRepository(SSOContext context) : base(context.Connection)
+        {
+            _context = context;
+        }
+
+        public List<ParceiroNegocioModulo> GetParceiroNegocioModulos(long cd_par, long cd_srv, long cd_mod)
+        {
+            var resultado = new List<ParceiroNegocioModulo>();
+            var parametros = new DynamicParameters();
+
+            parametros.Add("CD_PAR", cd_par, System.Data.DbType.Int16, System.Data.ParameterDirection.Input);
+            parametros.Add("CD_SRV", cd_srv, System.Data.DbType.Int16, System.Data.ParameterDirection.Input);
+            parametros.Add("CD_MOD", cd_mod, System.Data.DbType.Int16, System.Data.ParameterDirection.Input);
+
+            var query = @" SELECT * FROM TB_PAR_SRV_MOD WHERE TX_TABELA > @TX_TABELA";
+
+            resultado = _context.Connection.Query<ParceiroNegocioModulo>(query, parametros).ToList();
+
+            return resultado;
+        }
+    }
+}
