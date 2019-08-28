c1e1412a10e7bcc18fad5ae68b86d9453e85bc38
Julio <julio.cesar@versattily.com.br>
2019-08-28 16:30:23 -0300

    criação dos projetos inicais dos robos.

---
+++ Source/Automação/P2E.Automacao/P2E.Automacao.Core/Interfaces/IWork.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2E.Automacao.Core.Interfaces
+{
+    public interface IWork
+    {
+        void Processar();
+    }
+}
