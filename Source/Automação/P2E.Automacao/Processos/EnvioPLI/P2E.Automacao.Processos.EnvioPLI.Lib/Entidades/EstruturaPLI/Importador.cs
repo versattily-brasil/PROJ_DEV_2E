f3e4b773885635fb3f19383f4d3065963bfebd61
Júlio <julio.cesar@verssa>
2019-09-02 23:35:21 -0300

    Estrutura do aequixo.

---
+++ Source/Automação/P2E.Automacao/Processos/EnvioPLI/P2E.Automacao.Processos.EnvioPLI.Lib/Entidades/EstruturaPLI/Importador.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2E.Automacao.Processos.EnviarPLI.Lib.Entidades.EstruturaPLI
+{
+    [Serializable]
+    public class Importador
+    {
+        public int tipoImportador { get; set; }
+        public long cdImportador { get; set; }
+        public List<ItemMatriz> itemMatriz { get; set; }
+    }
+}
---
+++ Source/Automação/P2E.Automacao/Processos/EnvioPLI/P2E.Automacao.Processos.EnvioPLI.Lib/Entidades/EstruturaPLI/ItemMatriz.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2E.Automacao.Processos.EnviarPLI.Lib.Entidades.EstruturaPLI
+{
+    [Serializable]
+    public class ItemMatriz
+    {
+        public int cdNcmProdFinal { get; set; }
+        public int cdSuframa { get; set; }
+        public int cdDestinacao { get; set; }
+        public int cdUtilizacao { get; set; }
+        public string cdTributacao { get; set; }
+        public int numDecreto { get; set; }
+        public int tpDocumentoConcessivo { get; set; }
+        public int numDocumentoConcessivo { get; set; }
+        public int dtInicioBeneficio { get; set; }
+        public int dtFimBeneficio { get; set; }
+    }
+}
