e9f983400d8a0aa8a6f01dfac4e2f8189ab0fd49
Julio <julio.cesar@versattily.com.br>
2019-09-13 15:45:00 -0300

    Iniciando o projeto do orquestrador.

---
+++ Source/Automação/P2E.Automacao/Orquestrador/P2E.Automacao.Orquestrador.Lib/Util/Enum/eStatusAgendamento.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace P2E.Automacao.Orquestrador.Lib.Util.Enum
+{
+    public enum eStatusAgendamento
+    {
+        Pendente,
+        Executado_Com_Sucesso,
+        Executado_Com_Erro
+    }
+}
