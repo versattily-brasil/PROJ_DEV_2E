47edfcffb8c9254cccb06e55bde0080095c3ef0d
Lerron Felipe Correa de Jesus <lerron.felipe@versattily.com.br>
2019-10-06 14:07:36 -0400

    criação do projeto taxa de cambio

--- Source/Automação/P2E.Automacao/P2E.Automacao.Shared/Extensions/Geral.cs
+++ Source/Automação/P2E.Automacao/P2E.Automacao.Shared/Extensions/Geral.cs
@@ -1,22 +1,22 @@
-﻿using SimpleBrowser;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using SimpleBrowser;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace P2E.Automacao.Shared.Extensions
-{
-    public static class Geral
-    {
-        public static Browser CriarBrowser(string idCertificado)
-        {
-            Console.WriteLine("CARREGANDO O CERTIFICADO...");
-            var certificado = ControleCertificados.FindClientCertificate(idCertificado);
-            var browser = new Browser(certificado);
-            browser.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36";
+//namespace P2E.Automacao.Shared.Extensions
+//{
+//    public static class Geral
+//    {
+//        public static Browser CriarBrowser(string idCertificado)
+//        {
+//            Console.WriteLine("CARREGANDO O CERTIFICADO...");
+//            var certificado = ControleCertificados.FindClientCertificate(idCertificado);
+//            var browser = new Browser(certificado);
+//            browser.UserAgent = "Mozilla/5.0 (Windows NT 6.1; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.111 Safari/537.36";
 
-            return browser;
-        }
-    }
-}
+//            return browser;
+//        }
+//    }
+//}
---
+++ Source/Automação/P2E.Automacao/Processos/TaxaConversaoCambio/P2E.Automacao.TaxaConversaoCambio.Lib/Work.cs
@@ -0,0 +1,71 @@
+﻿using OpenQA.Selenium.PhantomJS;
+using P2E.Automacao.Entidades;
+using System;
+using System.Collections.Generic;
+using System.Net.Http;
+using System.Threading.Tasks;
+
+namespace P2E.Automacao.Processos.TaxaConversaoCambio.Lib
+{
+    public class Work
+    {
+        #region Variaveis Estáticas
+
+        public string _urlSite = @"https://www35.receita.fazenda.gov.br/tabaduaneiras-web/public/pages/security/login_publico.jsf";
+
+        private string _urlApiBase;
+        private List<Importacao> registros;
+        private string arquivoPath = "";
+        #endregion
+
+        public Work()
+        {
+            Console.WriteLine("#################  INICIALIZANDO - TAXA DE CONVERSÃO DE CAMBIO  ################# ");
+            _urlApiBase = System.Configuration.ConfigurationSettings.AppSettings["ApiBaseUrl"];
+        }
+
+        public async Task ExecutarAsync()
+        {
+            await CarregarListaDIAsync();
+        }
+
+        private async Task CarregarListaDIAsync()
+        {
+            using (var client = new HttpClient())
+            {
+                using (var service = PhantomJSDriverService.CreateDefaultService())
+                {
+                    service.AddArgument("test-type");
+                    service.AddArgument("no-sandbox");
+                    service.HideCommandPromptWindow = true;
+
+                    using (var _driver = new PhantomJSDriver(service))
+                    {
+                        try
+                        {
+                            Acessar(_driver);
+
+
+
+                            Console.ReadKey();
+                        }
+                        catch (Exception)
+                        {
+                            _driver.Close();
+                        }
+                    }
+                }
+
+            }
+        }
+
+        private async Task Acessar(PhantomJSDriver _driver)
+        {
+            Console.WriteLine("ACESSAO PAGINA DE CONSULTA...");
+            _driver.Navigate().GoToUrl(_urlSite);
+
+
+        }
+
+    }
+}
