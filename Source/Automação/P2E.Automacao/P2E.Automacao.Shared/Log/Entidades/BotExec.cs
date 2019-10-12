e8375159db49cea3eb4da69cc7c6cc2944efcf3d
Jullio Bicharra <julio.cesar@versattily.com.br>
2019-10-12 15:00:18 -0300

    ..

---
+++ Source/Automação/P2E.Automacao/P2E.Automacao.Shared/Log/Entidades/BotExec.cs
@@ -0,0 +1,30 @@
+﻿using MicroOrm.Dapper.Repositories.Attributes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using P2E.Automacao.Shared.Enum;
+
+namespace P2E.Automacao.Shared.Log.Entidades
+{
+    [Table("TB_BOT_EXEC")]
+    public class BotExec 
+    {
+        [Key]
+        [Identity]
+        public int CD_BOT_EXEC { get; set; }
+        public int CD_BOT { get; set; }
+        public int CD_AGENDA_EXEC { get; set; }
+        public eStatusExec OP_STATUS_BOT_EXEC { get; set; }
+        public int NR_ORDEM_EXEC { get; set; }
+
+        //public AgendaExec AgendaExec { get; set; }
+        //public Bot Bot { get; set; }
+
+        public DateTime? DT_INICIO_EXEC { get; set; }
+        public DateTime? DT_FIM_EXEC { get; set; }
+    }
+}
---
+++ Source/Automação/P2E.Automacao/P2E.Automacao.Shared/Log/Entidades/Map/BotExecMap.cs
@@ -0,0 +1,23 @@
+﻿using DapperExtensions.Mapper;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace P2E.Automacao.Shared.Log.Entidades.Map
+{
+    public class BotExecMap : ClassMapper<BotExec>
+    {
+        public BotExecMap()
+        {
+            Table("TB_BOT_EXEC");
+
+            Map(p => p.CD_BOT_EXEC).Column("CD_BOT_EXEC").Key(KeyType.Identity);
+            Map(p => p.CD_BOT).Column("CD_BOT");
+            Map(p => p.CD_AGENDA_EXEC).Column("CD_AGENDA_EXEC");
+            Map(p => p.NR_ORDEM_EXEC).Column("NR_ORDEM_EXEC");
+            Map(p => p.OP_STATUS_BOT_EXEC).Column("OP_STATUS_BOT_EXEC");
+            Map(p => p.DT_INICIO_EXEC).Column("DT_INICIO_EXEC");
+            Map(p => p.DT_FIM_EXEC).Column("DT_FIM_EXEC");
+        }
+    }
+}
